833b0b3d65e40926e8184117e83a32110d9eaff3
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-06-05 10:58:50 +0530

    updated

--- src/Axios.js
+++ src/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const Axios = axios.create({
-    // baseURL: "http://localhost:7000/api",
-    baseURL: "https://backend-project-zxa7.onrender.com/api",
+    baseURL: "http://localhost:7000/api",
+    // baseURL: "https://backend-project-zxa7.onrender.com/api",
 });
